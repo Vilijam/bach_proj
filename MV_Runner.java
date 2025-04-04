794a9b43b74c952f6194714eb44760f2afe689b7
William A. Carlsen <s214881@student.dtu.dk>
2025-04-04 17:25:45 +0200

    Genetic search fungerer. I gang med Gradient Descent

--- MV_Runner.java
+++ MV_Runner.java
@@ -5,10 +5,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.classfile.instruction.ThrowInstruction;
 import java.util.Arrays;
 import java.util.Random;
 
 public class MV_Runner {
+
+    String command;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -19,10 +22,13 @@ public class MV_Runner {
         String showPlots    = "-vp false ";
         String multiQuaTEx  = "-f "+MQadress+" ";
         String parallelism  = "-l 1 ";
-        String seed         = "-sots 1 ";
+        String seedOfSeeds  = "-sots 1 ";
         String significance = "-a 0.05 ";
+        String stateDescriptor = "-sd vesta.python.simpy.SimPyState ";
+        String blockSize    = "-bs 30 ";
+        String delta1       = "-d1 0.1 ";
 
-        String command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx + parallelism + seed + "-sd vesta.python.simpy.SimPyState " + showPlots  + "-bs 30 -d1 0.1 " + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python\" -ir 1 -mvad 7E-3 -wm 2 -pw 1 -nb 128 -ibs 8";
+        String command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx + parallelism + seedOfSeeds + stateDescriptor + showPlots + blockSize + delta1 + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python\" -ir 1 -mvad 7E-3 -wm 2 -pw 1 -nb 128 -ibs 8";
         String formals = "java -jar";
 
         command = formals + " " + jarFilePath + " " + command;
@@ -37,7 +43,108 @@ public class MV_Runner {
         update_Model(baseParameter);
         float[] baseline = run_One_MV(command);
 
+        Genetic_Search(baseline, baseParameter, command);
+
+    }
+
+    private static void Gradient_Descent(float[] baseline, int baseParameter, String command) throws InterruptedException, IOException{
         // init SEARCH
+        
+        Random rd = new Random();
+        int x0 = rd.nextInt(9);
+
+        // Ensure rd doesn't give the same int again.
+        while (x0 == baseParameter) {
+            x0 = rd.nextInt(9);
+        }
+
+        // eval x0
+        // choose direction and step size
+        // eval x1
+        // take step towards descent
+        // eval x2
+
+        // x2 less fit than x1
+        // reduce step size and choose direction
+        // eval new x2
+
+        // x1 less fit than x2
+        // continue descent until valley has been reached
+
+
+        System.out.println("Search starting at " + String.valueOf(x0));
+        
+        update_Model(x0);
+
+        float[] y0;
+        y0 = run_One_MV(command);
+
+        
+        int searchIteration = 0;
+        int stepSize = 2;
+        int stepDirection = 0;
+        while (stepDirection == 0) { 
+            stepDirection = rd.nextInt(3) - 1;
+        }
+
+        int xNext = x0 + stepDirection * stepSize;
+
+        if (0 > xNext || xNext > 8 && stepSize != 1) { 
+            stepDirection = stepDirection * -1;
+            xNext = x0 + stepDirection * stepSize;
+        }
+
+        float[] yNext = run_One_MV(command);
+        
+        double yNextFit = test_result(baseline, yNext);
+        double y0Fit = test_result(baseline, y0);
+
+        
+
+        // SEARCH
+        while (true) { 
+
+            System.out.println("Starting iteration: " + String.valueOf(searchIteration));
+
+            offspringGene = mutate(parentGene, rd); 
+            update_Model(offspringGene);
+
+            offspring = run_One_MV(command);
+            System.out.println("Offspring evaluation: " + Arrays.toString(offspring));
+
+            // Check null hypothesis
+            double offspringTStat = test_result(baseline, offspring); 
+            if (accept_null(offspringTStat)) {
+                searchIteration++; 
+                System.out.println("Gene of " + String.valueOf(offspringGene));
+                break; 
+            }
+            
+
+            // Check if mutation has better fitness. If so, it becomes parent.
+            double fitNew = offspringTStat;
+            double fitOld = test_result(baseline, parent);
+
+            if (fitNew < fitOld) { 
+                
+                System.out.println("Offspring won!");
+                parent = offspring; 
+                parentGene = offspringGene;
+            
+            } else {
+                System.out.println("Parent won!");
+            }
+            
+
+            searchIteration++;
+        }
+    }
+
+
+    private static void Genetic_Search(float[] baseline, int baseParameter, String command) throws InterruptedException, IOException{
+        // init SEARCH
+        
+        Random rd = new Random();
         int parentGene = rd.nextInt(9);
 
         // Ensure rd doesn't give the same int again.
@@ -56,6 +163,7 @@ public class MV_Runner {
 
 
         parent = run_One_MV(command);
+
         // SEARCH
         while (true) { 
 
@@ -67,60 +175,64 @@ public class MV_Runner {
             offspring = run_One_MV(command);
             System.out.println("Offspring evaluation: " + Arrays.toString(offspring));
 
-            // Test if new mean value is within CI of baseline CI
-            if (test_result(baseline, offspring)) {searchIteration++; break; }
-
-            
-            // Check if mutation has better fitness. If so, it becomes parent.
-            if (searchIteration != 0) {
-                float fitNew = fitness_function(baseline, offspring);
-                float fitOld = fitness_function(baseline, parent);
-
-                if (fitNew > fitOld) { 
-                    
-                    System.out.println("Offspring won!");
-                    parent = offspring; 
-                    parentGene = offspringGene;
-                
-                } else {
-                    System.out.println("Parent won!");
-                }
+            // Check null hypothesis
+            double offspringTStat = test_result(baseline, offspring); 
+            if (accept_null(offspringTStat)) {
+                searchIteration++; 
+                System.out.println("Gene of " + String.valueOf(offspringGene));
+                break; 
             }
+            
+
+            // Check if mutation has better fitness. If so, it becomes parent.
+            double fitNew = offspringTStat;
+            double fitOld = test_result(baseline, parent);
+
+            if (fitNew < fitOld) { 
+                
+                System.out.println("Offspring won!");
+                parent = offspring; 
+                parentGene = offspringGene;
+            
+            } else {
+                System.out.println("Parent won!");
+            }
+            
 
             searchIteration++;
         }
-
-        // Create population
-        
-        // Run MV
-        // Calculate fitness of population
-        // Cull and multiply
-        // Mutate
-        // Repeat
-
-
     }
 
     private static int mutate(int parent, Random rd) {
-        int change = rd.nextInt(7) - 3;
-        int offspring = Math.max(0,parent + change);
+        
+        // Ensuring offspring is different from parent
+        // Ensuring gene € [0,8]
+        int change = 0;
+        while (change == 0 || 0 > parent + change || parent + change > 8) { 
+            change = rd.nextInt(7) - 3;             
+        }
+
+        int offspring = parent + change;
+        
         System.out.println("Offspring mutated by " + String.valueOf(change) + " resulting in parameter " + String.valueOf(offspring));
         return offspring;
     }
 
-    private static float fitness_function(float[] target, float[] geneExpression) {
-        
-        return target[0] - geneExpression[0]; 
+    //Assuming n = 30 and significance of 0.05
+    private static double test_result(float[] base, float[] model) {
+        double dut = (base[0] - model[0])/ Math.sqrt((base[1] + model[1])/30);
+        System.out.println("Testing model evaluation resulted in t stat: " + String.valueOf(dut));
+        return dut;
     }
 
-    private static boolean  test_result(float[] base, float[] model) {
-        return base[0] - base[2] < model[0] && model[0] < base[0] + base[2];
+    //Assuming 60 dof instead of 58, and significance of 0.05
+    private static boolean accept_null(double t_stat) {
+        return t_stat <= 2;
     }
 
     public static float[] run_One_MV(String command) throws InterruptedException, IOException {
         ProcessBuilder pb = new ProcessBuilder(command.split(" "));
                     
-
         Process p = pb.start();
 
         // Read the output of the process
@@ -128,9 +240,12 @@ public class MV_Runner {
         String line;
         String result = "";
         while ((line = reader.readLine()) != null) {
+            //System.out.println(line);
             if (line.equals(" steady-state value of MyProperty(avgHappy_Noise):")) {
                 
                 result = reader.readLine();
+                //System.out.println(result);
+                
             }
         }
 
