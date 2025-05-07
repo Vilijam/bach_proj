046a2d9ace81e341a2712cabe38c4d63cb37ebba
William A. Carlsen <s214881@student.dtu.dk>
2025-05-07 17:35:53 +0200

    Mostly implemented MOEA/d i think

--- MV_Runner.java
+++ MV_Runner.java
@@ -30,37 +30,227 @@ public class MV_Runner {
 
  
 
-        String schelling_command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx_schelling + parallelism + seedOfSeeds + stateDescriptor + showPlots + blockSize + delta1 + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python\" -ir 1 -mvad 7E-3 -wm 2 -pw 1 -nb 128 -ibs 8";
-        String boid_command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx_boids + parallelism + seedOfSeeds + stateDescriptor + showPlots + blockSize + delta1 + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python\" -ir 1 -ms 600";
+        String schelling_command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx_schelling + parallelism + seedOfSeeds + stateDescriptor + showPlots + blockSize + delta1 + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python\" -ir -1 -mvad 7E-3 -wm 2 -pw 1 -nb 128 -ibs 8";
+        String boid_command =  "-c -m schelling/MV_python_integrator_schelling.py -sm true " + multiQuaTEx_boids + parallelism + seedOfSeeds + stateDescriptor + showPlots + blockSize + delta1 + significance + "-otherParams \"/Users/William/AppData/Local/Programs/Python/Python313/python homophily 3 \" -ir 1 -ms 600";
         String formals = "java -jar";
 
         schelling_command = formals + " " + jarFilePath + " " + schelling_command;
         boid_command =   formals + " " + jarFilePath + " " + boid_command;
 
- 
-        // Parameterisation
-        // Comparison
-
-        // Initialisation - 
-        // Evaluation - fitness er t-test?
-        // Terminate - inden for CI?
-
-        // Selection
-        // Variation
-        
-        // BASELINE
         Random rd = new Random();
-        //int baseParameter = rd.nextInt(9);
-        int baseParameter = 2;
 
-        System.out.println("Base parameter set to " + String.valueOf(baseParameter));
-        update_Model(baseParameter);
-        float[] baseline = run_One_MV(schelling_command);
 
-        Genetic_Search(baseline, baseParameter, schelling_command);
+        /*
+         // BASELINE
+         int baseParameter = rd.nextInt(9);
+
+         System.out.println("Base parameter set to " + String.valueOf(baseParameter));
+         update_Model(baseParameter);
+         float[] baseline = run_One_MV(schelling_command);
+ 
+         Genetic_Search(baseline, baseParameter, schelling_command);
+
+         */
+
+        
+        int m = 8 ; // objectives
+        int N = 10; // Amount of sub-problems and pop-size
+        int T = 3;  // neighboors
+        double[] target = new double[m]; 
+
+
+        double[][] gamer = generate_weight_vectors(rd, N, m);
+
+        for (int i = 0; i < N; i++) {
+
+            System.out.println(Arrays.toString(gamer[i]));
+            
+        }
+
+        int[][] nbh = find_weight_vector_neighbs(T, gamer);
+
+        double[] poop = {22, 0.5, 1000};
+        double[][] pop = generate_population(N, poop, rd);
+
+        System.out.println(Arrays.toString(poop));            
+
+        for (double[] pop1 : pop) {
+            System.out.println(Arrays.toString(pop1));
+        } 
 
     }
 
+    private static double[][] moead_loop(Random rd, int N, int m, int T, int[][] nbh, double[][] pop) {
+        double[][] external_pop = new double[N][m];
+
+        for (int i = 0; i < N; i++) {
+            int[] nbs = nbh[0];
+            int first_parent = rd.nextInt(nbs.length);
+            int second_parent = rd.nextInt(nbs.length);
+            while (second_parent == first_parent) {second_parent = rd.nextInt(nbs.length);}
+
+            double[] child = genetic_operation(pop[nbs[first_parent]], pop[nbs[second_parent]], rd);
+
+
+            // repair child?
+
+            // eval child
+            
+            // update z
+
+
+            
+
+
+
+
+            
+
+        }
+
+        return external_pop;
+    }
+
+    private static double[] genetic_operation(double[] x1, double[] x2, Random rd) {
+
+        double mutation_rate = 0.05;
+        int crossover_point = rd.nextInt(x1.length);
+
+        double[] child = new double[x1.length];
+        
+        // Crossover of the two parents
+        for (int i = 0; i < x1.length; i++) {
+            if (i < crossover_point) {
+                child[i] = x1[i];
+            } else {
+                child[i] = x2[i];
+            } 
+        }
+        // Bullshit mutation
+        for (int i = 0; i < child.length; i++) {
+            if (rd.nextDouble() < mutation_rate) {
+                
+                child[i] = child[i] * 1.1;         
+            }   
+        }
+        return child;
+    }
+
+    private static double[][] generate_population(int popCount, double[] ancestor, Random rd) {
+
+        // mutations to generate a population from an ancestor
+        double[] mutations = { 1.2,0.75, 1.5,0.5, 1.8,0.2, 1.1,0.9, 1.3,0.7};
+
+        double[][] population = new double[popCount][ancestor.length];
+        population[0] = ancestor;
+
+        for (int i = 1; i < popCount; i++) {
+
+            // 50% to mutate any gene of the ancestor
+            double[] next_child = ancestor.clone();
+            for (int j = 0; j < ancestor.length; j++) {
+
+                if (rd.nextInt(2) == 0) {
+                    next_child[j] = next_child[j] * mutations[rd.nextInt(10)];
+                }
+            }
+
+            population[i] = next_child;
+        }
+
+        return population;
+
+    }
+
+    // Assuming one and two lenght =
+    private static double euclidean_distance(double[] one, double[] two) {
+
+        double sum = 0;
+        for (int i = 0; i < one.length; i++) {
+            sum = Math.pow((one[i] - two[i]), 2);
+        }
+
+        return Math.sqrt(sum);
+    }
+
+    // Calculating the B function. Vector Neighboorhood
+    private static int[][] find_weight_vector_neighbs(int T, double[][] weightVectors) {
+
+        // B(i). 1st level is all vectors. 2nd is the T closest neighboors' index in weighVectors
+        int[][] neighboorArray = new int[weightVectors.length][T];
+
+
+        // GO through all vectors
+        for (int i = 0; i < weightVectors.length; i++) {
+            
+
+            // Find ith vectors distance to all others. -- replicates work dist(a,b) = dist(b,a). Distance matrix?
+            double[] distances = new double[weightVectors.length];
+            for (int j = 0; j < weightVectors.length; j++) {
+                distances[j] = euclidean_distance(weightVectors[i], weightVectors[j]);
+            }
+
+            
+            // the T lowest ideces
+            int[] indeces_of_T_lowest_dists = new int[T];
+
+            // init'ing closest neighboors array
+            for (int j = 0; j < T; j++) {
+                indeces_of_T_lowest_dists[j] = j;
+            }
+
+            // finding closest neighboors. Assuming T is larger than count of WV
+            for (int j = T; j < distances.length; j++) {
+                
+                // find the worst neighboor in the neighboorhood. --replicate work. When curr WV is further, the furthest neighboor does not change.
+                int furthestNeigh = 0;
+                for (int l = 1; l < T; l++) {
+                    if (distances[indeces_of_T_lowest_dists[furthestNeigh]] < distances[indeces_of_T_lowest_dists[l]]) {
+                        furthestNeigh = l;
+                    } 
+                }
+
+                // Update nbh if curr wv is closer
+                if (distances[j] < distances[indeces_of_T_lowest_dists[furthestNeigh]]) {
+                    indeces_of_T_lowest_dists[furthestNeigh] = j;
+                }
+            }
+            
+            // append nbh is nbh array
+            neighboorArray[i] = indeces_of_T_lowest_dists;
+
+        }
+
+        return neighboorArray;
+    }
+
+    private static double[][] generate_weight_vectors(Random rd, int vCount, int oCount) {
+
+        double[][] weightVectors = new double[vCount][oCount];
+        
+        for (int i = 0; i < vCount; i++) {
+            
+
+            double[] steps = new double[oCount];
+            for (int j = 0; j < oCount-1; j++) {
+                steps[j] = rd.nextFloat();
+            }
+
+            steps[oCount-1] = 1;
+
+            Arrays.sort(steps);
+        
+            double floor = 0;
+            for (int j = 0; j < oCount; j++) {
+                
+                weightVectors[i][j] = steps[j] - floor;
+
+                floor = steps[j];
+            }
+        }
+
+        return weightVectors;
+    } 
 
     private static void Genetic_Search(float[] baseline, int baseParameter, String command) throws InterruptedException, IOException{
         // init SEARCH
@@ -144,12 +334,12 @@ public class MV_Runner {
     public static double test_result(float[] base, float[] model) {
         double dut = (base[0] - model[0])/ Math.sqrt((base[1] + model[1])/30);
         //System.out.println("Testing model evaluation resulted in t stat: " + String.valueOf(dut));
-        return dut;
+        return Math.abs(dut);
     }
 
     //Assuming 60 dof instead of 58, and significance of 0.05
     private static boolean accept_null(double t_stat) {
-        return Math.abs(t_stat) <= 2;
+        return t_stat <= 2;
     }
 
     public static float[] run_One_MV(String command) throws InterruptedException, IOException {
